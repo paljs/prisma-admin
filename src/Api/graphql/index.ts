f3887e612b30f67ed9a5839369afc725bbcea057
Ahmed Elywa <ahmed.elywa@icloud.com>
2020-06-05 01:22:28 +0200

    update to last admin package

--- src/Api/graphql/index.ts
+++ src/Api/graphql/index.ts
@@ -2,5 +2,5 @@ export * from './models'
 export * from './inputTypes'
 export * from './Auth'
 
-import { adminNexusSettings } from '@prisma-tools/admin/dist/nexus'
+import { adminNexusSettings } from '@prisma-tools/nexus/dist/adminSettings'
 export const adminSettings = adminNexusSettings()
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Settings } from '@prisma-tools/admin';
+import { Settings } from '@prisma-tools/admin/dist/Settings';
 
 export default function SettingsPage() {
   return <Settings />;
