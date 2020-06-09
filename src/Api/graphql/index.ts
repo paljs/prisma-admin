8bc90d5ea4867323924b0e05b2e517e4eecbecf6
Ahmed Elywa <ahmed.elywa@icloud.com>
2020-06-09 12:08:20 +0200

    update nexus version

--- src/Api/graphql/index.ts
+++ src/Api/graphql/index.ts
@@ -2,5 +2,5 @@ export * from './models'
 export * from './inputTypes'
 export * from './Auth'
 
-import { adminNexusSettings } from '@prisma-tools/nexus/dist/adminSettings'
+import { adminNexusSettings } from '@prisma-tools/nexus'
 export const adminSettings = adminNexusSettings()
