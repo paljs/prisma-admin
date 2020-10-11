36bcb8ec2b5202909d9852f768fb40bc676f8e50
Ahmed Elywa <ahmed.elywa@icloud.com>
2020-10-11 12:57:17 +0200

    add google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,18 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
         <title>Prisma Admin Dynamic Table</title>
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171177495-2"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'UA-171177495-2');
+              `,
+          }}
+        />
       </Head>
       <ApolloProvider client={apolloClient}>
         <Layout>
