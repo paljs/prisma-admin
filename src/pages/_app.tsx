b7ba650bd2deb7ebfb223d17c56d97fc955e5f5e
Ahmed Elywa <ahmed.elywa@icloud.com>
2021-06-27 18:36:22 +0200

    add font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,6 +21,7 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
         <title>Great PalJs App</title>
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       </Head>
       <ApolloProvider client={apolloClient}>
         {admin ? (
